189583fd04181462c14ea41c638a2f1beb4e720d
Luis Felipe Mendivelso Osorio <lf.mendivelso10@uniandes.edu.co>
2016-03-15 08:44:46 -0500

    Version 0.0.1 - Sprint1-Latency-Final

--- App_Shutdown.sh
+++ App_Shutdown.sh
@@ -3,5 +3,7 @@
 # Grupo: 1
 # 2016-03-09
 #
+sudo service postgresql start
+sudo service mongod start
 bundle exec sidekiq -d -l ./log/sidekiq.log
 thin stop -P tmp/pids/thin.pid
--- App_Start.sh
+++ App_Start.sh
@@ -3,5 +3,7 @@
 # Grupo: 1
 # 2016-03-09
 #
+sudo service postgresql stop
+sudo service mongod stop
 bundle exec sidekiq -d -l ./log/sidekiq.log
 thin start -p 3000 -P tmp/pids/thin.pid -l log/server.log -d -e production
