a3dfc7ae2c715facb51d7322cfa1725680b1435a
Luis Felipe Mendivelso Osorio <lf.mendivelso10@uniandes.edu.co>
2016-04-10 20:11:44 -0500

    Corrección de Conexiones

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -17,7 +17,7 @@ redis_conn = proc {
 # Server configuration por 250
 Sidekiq.configure_server do |config|
   config.redis = {url: 'redis://localhost:6379/12'}
-  config.redis = ConnectionPool.new(size: 250, &redis_conn)
+  config.redis = ConnectionPool.new(size: 25, &redis_conn)
 
 end
 # Client Configuration
